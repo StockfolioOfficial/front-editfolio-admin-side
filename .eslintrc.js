628fe98e4759d35551a66b3eedc99cdbe97f4766
Developer-Mang <chiwon618@gmail.com>
2021-11-02 16:04:03 +0900

    feat: favicon 추가 및 eslint consistent-return role 비활성화

--- .eslintrc.js
+++ .eslintrc.js
@@ -41,6 +41,7 @@ module.exports = {
     '@typescript-eslint/explicit-module-boundary-types': 0,
     '@typescript-eslint/no-unused-vars': ['error'],
     '@typescript-eslint/no-var-requires': 0,
+    'consistent-return': 0,
     'prettier/prettier': [
       'error',
       {
